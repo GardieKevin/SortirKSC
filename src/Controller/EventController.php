9b082d4e6678a8236a4b07a48bd198779da0a5c6
kgardie2021 <kevin.gardie2021@campus-eni.fr>
2022-04-12 10:12:55 +0200

    MAJ: creation page "create event"

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,10 +22,26 @@ class EventController extends AbstractController
 //    }
 
     #[Route('/create', name: 'event_create')]
-    public function create(): Response
-    {
-        return $this->render('event/create.html.twig',
+    public function create(
+        Request $request,
+        EntityManagerInterface $em,
 
+    ): Response
+    {
+        $event = new Event();
+        $eventForm = $this->createForm(EventType::class, $event);
+        $eventForm->handleRequest($request);
+
+        if ($eventForm->isSubmitted() && $eventForm->isValid()){
+            $em->persist($event);
+            $em->flush();
+            $this->addFlash('info', 'Event successfully created !');
+            return $this->redirectToRoute('main_home');
+        }
+
+        return $this->render(
+            'event/create.html.twig',
+            ['myForm' => $eventForm->createView()]
         );
     }
 }
