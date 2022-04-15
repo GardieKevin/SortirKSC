fc0d3b3ecf27b2435b69cc02683b4b892f79e26f
selie2021 <sebastien.elie@hotmail.fr>
2022-04-15 17:23:57 +0200

    Seb nouveau pseudo pour crééer un utilisateur

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,11 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use phpDocumentor\Reflection\Types\Self_;
+
 class UserCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -15,13 +19,27 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    public function configureFields(string $pageName): iterable
+    public function configureFields(string $pageName ): iterable
     {
+
+
+        // Initialisation des caractères utilisables
+//        $characters = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+//
+//        for($i=0;$i<10;$i++)
+//        {
+//            $password= $i?($characters[array_rand($characters)]):$characters[array_rand($characters)];
+//            $pass = implode("", );
+//            var_dump($password);
+//        }
+
+
         return [
             // IdField::new('id'),
             TextField::new('firstname'),
             TextField::new('lastname'),
             TextField::new('pseudo'),
+            TextField::new('password', 'password')->setDisabled(),
             TextField::new('phone'),
             TextField::new('email'),
             AssociationField::new('campus'),
@@ -30,3 +48,4 @@ class UserCrudController extends AbstractCrudController
         ];
     }
 }
+
