c0554a6805fe07f220b01e89913d6b6a813300c0
selie2021 <sebastien.elie@hotmail.fr>
2022-04-15 13:58:22 +0200

    Seb rajout du champs pseudo

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Event;
 use App\Entity\User;
+use mysql_xdevapi\RowResult;
 use SebastianBergmann\CodeCoverage\Report\Text;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -44,7 +45,9 @@ class UserType extends AbstractType
             ->add('lastname', TextType::class,
                 ['label'=>'Surname '])
             ->add('firstname', TextType::class,
-                ['label'=>'Given name'])
+                ['label'=>'firstname'])
+            ->add('pseudo', TextType::class,
+            ['label'=>'Pseudo'])
             ->add('phone', TextType::class,
                 ['label'=>'Phone'])
             ->add('photo',FileType::class,
