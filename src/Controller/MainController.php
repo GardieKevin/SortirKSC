b53050e5e773f94024d1aa29fbf585fbb66ad8ea
selie2021 <sebastien.elie@hotmail.fr>
2022-04-15 10:35:54 +0200

    Seb changement d'état en fonction de la date du jour

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use function MongoDB\BSON\toJSON;
 use function Sodium\add;
 
+
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main_home')]
@@ -28,23 +29,48 @@ class MainController extends AbstractController
         $etat = $etatRepository->find($id);
 
         $events = $eventRepository->findAllEvents();
-        foreach ($events as $e){
-        if (new \DateTime('now') > $e->getStartingDate())
-            {$id=3;
-                $etat = $etatRepository->find($id);
+        foreach ($events as $e) {
+            $eventDate = $e->getStartingDate();
+            $today=new \DateTime('now');
+            $limitDate = $e->getLimitInscribeDate();
+//            dd($date->modify('+ '.$e->getDuration().'days'));
+            if ($today> $eventDate)
+            {
+                if ($today < $eventDate->modify('+ ' . $e->getDuration() . 'days')) {
+                    $id = 4;
+                    $etat = $etatRepository->find($id);
 
-                $e->setEtat($etat);
-                $entityManager->persist($e);
-                $entityManager->flush();
+                    $e->setEtat($etat);
+                    $entityManager->persist($e);
+                    $entityManager->flush();
+                }
+                else{
+                    $id = 5;
+                    $etat = $etatRepository->find($id);
+                    $e->setEtat($etat);
+                    $entityManager->persist($e);
+                    $entityManager->flush();
+                }
             }
-//        if(new \DateTime('now') > $e->getStartingDate() and (new \DateTime('now'))->date_add($e->getDuration()) < $e->getStartingDate())
-//        {
-//            $id=4;
-//            $etat = $etatRepository->find($id);
-//            $e->setEtat($etat);
-//            $entityManager->persist($e);
-//            $entityManager->flush();
-//        }
+            if($today<$eventDate) {
+                if ($today < $limitDate)
+                {
+                    $id = 2;
+                    $etat = $etatRepository->find($id);
+                    $e->setEtat($etat);
+                    $entityManager->persist($e);
+                    $entityManager->flush();
+                }
+
+                else
+                {
+                    $id = 3;
+                    $etat = $etatRepository->find($id);
+                    $e->setEtat($etat);
+                    $entityManager->persist($e);
+                    $entityManager->flush();
+                }
+            }
         }
         return $this->render('main/home.html.twig',
             compact("events")
