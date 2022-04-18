1d5eaa8cd861a66b6d913f43cb02bcee3aab5fee
kgardie2021 <kevin.gardie2021@campus-eni.fr>
2022-04-19 00:30:49 +0200

    nouvelle MAJ barre rechercher javascript

--- public/js/scriptSearch.js
+++ public/js/scriptSearch.js
@@ -32,51 +32,62 @@ function myMethod() {
                     eventsTaken.push(result);
                 }
             }
+        } else{
+            for (let result of results){
+                console.log("Je prends cet event comme tous les autres");
+                eventsTaken.push(result);
+            }
         }
         console.log(eventsTaken);
 
+        let campus = document.getElementById('campus');
+        let name = document.getElementById('name');
+        let dateStart = document.getElementById('dateStart').valueAsDate;
+        let dateEnd = document.getElementById('dateEnd').valueAsDate;
 
+        let eventSearched = [];
+
+        if (dateStart === null) {
+            dateStart = Date.now();
+        }
+        if (dateEnd === null) {
+            dateEnd = Date.now() + 86400 * 1000 * 365;
+        }
+
+        $(event).html("")
+
+        for (let event of eventsTaken) {
+            let date = new Date(event['startingDate']).getTime();
+
+            if (event['name'].toLowerCase().includes(name.value.toLowerCase())
+                && event['campus']['name'].includes(campus.value)
+                && date > dateStart
+                && date < dateEnd
+            ){
+                eventSearched.push(event);
+
+                //Construction de l'event
+//                 $(event).append('<h2>' + "Name : " + result['name'] + '</h2>',
+//                     '<div>' + "Date & Hour : " + result['startingDate'] + '</div>',
+//                     '<div>' + "Duration : " + result['duration'] + '</div>',
+//                     '<div>' + "Inscription limit : " + result['limitInscribeDate'] + '</div>',
+//                     '<div>' + "Max affluence : " + result['maxInscriptionsNumber'] + '</div>',
+//                     '<div>' + "Informations : " + result['informations'] + '</div>',
+//                     '<a href="user/' + result['organisator']['id'] + '">' + result['organisator']['pseudo'] + '</a>',
+//                     '<div>' + "Campus : " + result['campus']['name'] + '</div>',
+//                     '<a href="event/detail/' + result['id'] + '"><button type="submit"> Détails </button></a>',
+            }
+        }
+        console.log(eventSearched);
     })
 }
 
-let idParticipant;
-if (document.getElementById("participant").checked === true){
-    idParticipant = parseInt(document.getElementById('idUser').value);
-}
-console.log(idParticipant);
+//let idParticipant;
+//if (document.getElementById("participant").checked === true){
+//    idParticipant = parseInt(document.getElementById('idUser').value);
+//}
+//console.log(idParticipant);
 
-let campus = document.getElementById('campus');
-let name = document.getElementById('name');
-let dateStart = document.getElementById('dateStart').valueAsDate;
-let dateEnd = document.getElementById('dateEnd').valueAsDate;
-
-let eventSearched = [];
-
-if (dateStart === null) {
-    dateStart = Date.now();
-}
-if (dateEnd === null) {
-    dateEnd = Date.now() + 86400 * 1000 * 365;
-}
-
-fetch(apiUrl, {method: 'get'}).then(response => response.json()).then(results => {
-
-    //mise à blanc de la liste des events
-    $(event).html("")
-
-    for (let result of results) { //TODO remplacer le result par event et results par le tableau eventsTaken
-        let date = new Date(result['startingDate']).getTime();
-
-        if (result['name'].toLowerCase().includes(name.value.toLowerCase())
-            && result['campus']['name'].includes(campus.value)
-            && date > dateStart
-            && date < dateEnd
-        ){
-            eventSearched.push(result);
-        }
-    }
-})
-console.log(eventSearched);
 
 
 //
