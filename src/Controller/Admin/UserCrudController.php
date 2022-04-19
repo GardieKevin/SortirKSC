ecf7c1276c98ec5a949932bffda5e126aeae0b5f
selie2021 <sebastien.elie@hotmail.fr>
2022-04-19 10:23:59 +0200

    Seb ajout mot de passe autogénéré dans dashboard

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -8,12 +8,16 @@ use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use phpDocumentor\Reflection\Types\Self_;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+
 
 class UserCrudController extends AbstractCrudController
 {
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -23,7 +27,7 @@ class UserCrudController extends AbstractCrudController
     {
         // Initialisation des caractères utilisables
         $characters = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-
+        $password='';
         for($i=0;$i<$size;$i++)
         {
             $password .= ($i%2) ? strtoupper($characters[array_rand($characters)]) : $characters[array_rand($characters)];
@@ -35,14 +39,14 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName ): iterable
     {
 
-
+        $pass = $this->Genere_Password(10);
 
         return [
             // IdField::new('id'),
             TextField::new('firstname'),
             TextField::new('lastname'),
             TextField::new('pseudo'),
-            TextField::new('password', 'password')->setDisabled(),
+            TextField::new('password','password')->setFormTypeOption('data', $pass)->setDisabled(),
             TextField::new('phone'),
             TextField::new('email'),
             AssociationField::new('campus'),
