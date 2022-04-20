5caec47d82dd5d687f1d1ecf7221b5127fe65584
selie2021 <sebastien.elie@hotmail.fr>
2022-04-20 11:57:23 +0200

    Seb archivage + mot de passe sur appli pour creation de user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,6 +16,19 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
+    function Genere_Password($size)
+    {
+        // Initialisation des caractères utilisables
+        $characters = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+        $password='';
+        for($i=0;$i<$size;$i++)
+        {
+            $password .= ($i%2) ? strtoupper($characters[array_rand($characters)]) : $characters[array_rand($characters)];
+        }
+
+        return $password;
+    }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
@@ -27,12 +40,13 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-            $user->setPassword(
-            $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+            $password=$this->Genere_Password(10);
+            $user->setPassword($password);
+//            $userPasswordHasher->hashPassword(
+//                    $user,
+//                    $form->get('plainPassword')->getData()
+//
+//            );
 
             $photo = $form->get('photo')->getData();
 
