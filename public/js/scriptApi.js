3375c9503f87658928cd25fba1a70885898294fc
Corentinlgn <corentin.longein2021@campus-eni.fr>
2022-04-20 15:10:29 +0200

    MAP avec tout c'est gratuit

--- public/js/scriptApi.js
+++ public/js/scriptApi.js
@@ -1,14 +1,14 @@
+let coordonneesX = null;
+let coordonneesY = null;
 $(document).ready(function () {
     let apiUrl = 'https://geo.api.gouv.fr/communes?codePostal=';
-
-    const formatPostcode = '&format=json' + '&boost=population&limit=5';
-
+    const formatPostcode = '&format=json' + '&boost=population&limit=3';
     let searchPostCode = $('#searchPostCode');
     let postcode = $('#postcode')
     let errorMessage = $('error-message');
 
-    /*Recherche de la commune avec autocompletion */
 
+    /* ------------Recherche API avec CODE POSTAL écrit par USER !!!! ------------*/
     $(searchPostCode).on('keyup', function () {
 
         let code = document.getElementById('searchPostCode').value;
@@ -22,7 +22,7 @@ $(document).ready(function () {
                 $.each(results, function (key, value) {
                     //console.log(value);
                     //console.log(value.nom);
-                    $(postcode).append('<option value="' + value.nom+ '" id="' + value.nom + '">' + value.nom +'</option>');
+                    $(postcode).append('<option value="' + value.nom + '" id="streetId">' + value.nom + '</option>');
 
                 });
             } else {
@@ -41,33 +41,36 @@ $(document).ready(function () {
 
     let apiUrlStreet = 'https://api-adresse.data.gouv.fr/search/?q=';
     const formatStreet = '&format=json';
-
     let searchStreet = $('#searchStreet');
     let street = $('#street');
+    let coordonneeX = $('#coordonneesX');
+    let coordonneeY = $('#coordonneesY');
 
+    /* ------------Recherche API avec le code POSTAL renseigné au dessus !!!! ------------*/
+    $(searchStreet).on('keyup', function () {
 
-    $(searchStreet).on('blur', function (){
-        let codeStreet = document.getElementById('searchStreet').value;
+        let valueStreet = document.getElementById('searchStreet').value;
         let codePostCode = document.getElementById('searchPostCode').value;
-        let url = apiUrlStreet + codeStreet + '&postcode='+ codePostCode +formatStreet;
-
+        let url = apiUrlStreet + valueStreet + '&postcode=' + codePostCode + formatStreet;
 
         fetch(url, {method: 'get'}).then(response => response.json()).then(results => {
 
             $(street).find('option').remove();
+            $(coordonneeX).find('option').remove();
+            $(coordonneeY).find('option').remove();
             if (results) {
+                results.features.forEach((features) => {
+                    $(street).append('<option value="' + features.properties.name + '" id="' + features.properties.name + '">' + features.properties.name + '</option>');
 
-                results.features.forEach((features)=>{
-                    console.log(features.label)
-                        $(street).append('<option value="' + features.properties.name + '" id="' + features.properties.name + '">' + features.properties.name +'</option>');
-
+                    /*$(coordonneeX).append('<option value="' + features.geometry.coordinates[1] + '" id="' + coordonneesX + '">' + features.geometry.coordinates[1] + '</option>');
+                    $(coordonneeY).append('<option value="' + features.geometry.coordinates[0] + '" id="' + coordonneesY + '">' + features.geometry.coordinates[0] + '</option>');*/
                 })
             } else {
                 if ($(street).val()) {
                     console.log('Erreur de code postal.');
                     $(errorMessage).text('Aucune commune avec ce code postal.').show();
                 } else {
-                    $(errorMessage).text('').hide()
+                    $(errorMessage).text('').hide();
                 }
             }
         }).catch(err => {
@@ -76,10 +79,50 @@ $(document).ready(function () {
         });
 
     })
+})
+
+function valCommune() {
+    let commune = document.getElementById("postcode").value;
+
+}
+
+function valRue() {
+    let rue = document.getElementById("street").value;
+    coordonnees(rue);
+}
+
+function coordonnees(rue) {
+    let valueStreet = rue;
+    console.log(valueStreet);
+    let codePostCode = document.getElementById('searchPostCode').value;
+    let apiUrlStreet = 'https://api-adresse.data.gouv.fr/search/?q=';
+    const formatStreet = '&format=json&limit=1';
+    let url = apiUrlStreet + valueStreet + '&postcode=' + codePostCode + formatStreet;
+    let divCoordonnee = $('#divCoordonnee');
+    $(divCoordonnee).find('div').remove();
+    fetch(url, {method: 'get'}).then(response => response.json()).then(results => {
 
 
+        if (results) {
+            results.features.forEach((features) => {
+
+                $(divCoordonnee).append('<div value="' + features.geometry.coordinates[1] + '" id="coordonneesY">' + features.geometry.coordinates[1] + '</div>');
+                coordonneesY = features.geometry.coordinates[1];
+                $(divCoordonnee).append('<div value="' + features.geometry.coordinates[0] + '" id="coordonneesX">' + features.geometry.coordinates[0] + '</div>');
+                coordonneesX = features.geometry.coordinates[0];
+            })
+        } else {
+            if ($(street).val()) {
+                console.log('Erreur de code postal.');
+                $(errorMessage).text('Aucune commune avec ce code postal.').show();
+            } else {
+                $(errorMessage).text('').hide();
+            }
+        }
+    }).catch(err => {
+        console.log(err);
+        $(postcode).find('select').remove();
+    });
 
 
-
-
-});
+}
