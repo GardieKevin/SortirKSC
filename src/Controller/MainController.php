aca25e68c18a3fec2dd2728e405261f20cb55620
Kévin Gardie <99649947+GardieKevin@users.noreply.github.com>
2022-04-20 12:00:13 +0200

    Seb branch (#77)
    * Seb changement etat en cloturé
    
    * Seb changement d'état en fonction de la date du jour
    
    * Seb changement d'état en fonction de la date du jour
    
    * Seb changement d'état en fonction de la date du jour
    
    * Seb rajout du champs pseudo
    
    * Seb nouveau pseudo pour crééer un utilisateur
    
    * Seb ajout etat event dans dashboard
    
    * Seb ajout mot de passe autogénéré dans dashboard
    
    * Seb ajout mot de passe autogénéré dans dashboard 2eme
    
    * Seb possibilité de s'inscrire à un evenement
    
    * Seb possibilité dcloturé un evenement
    
    * Seb possibilité de se désinscrire d'un evenement
    
    * Seb archivage + mot de passe sur appli pour creation de user
    
    Co-authored-by: selie2021 <sebastien.elie@hotmail.fr>

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -27,47 +27,82 @@ class MainController extends AbstractController
 
         $id = 3;
         $etat = $etatRepository->find($id);
-
+        $today = new \DateTime('now');
+        $archiveDate = 60;
         $events = $eventRepository->findAllEvents();
-        foreach ($events as $e) {
-            if ($eventEtat = $e->getEtat()->getId() != 6) {
+//        dd($events);
+        foreach ($events as $e)
+        {
                 $eventDate = $e->getStartingDate();
-                $today = new \DateTime('now');
                 $limitDate = $e->getLimitInscribeDate();
-//            dd($eventDate->modify(+ $e->getDuration().'days'));
-                if ($today > $eventDate) {
-                    if ($today < $eventDate->modify('+ ' . $e->getDuration() . 'days')) {
-                        $id = 4;
-                        $etat = $etatRepository->find($id);
+                if ($eventEtat = $e->getEtat()->getId() != 6)
+                {
 
-                        $e->setEtat($etat);
-                        $entityManager->persist($e);
-                        $entityManager->flush();
-                    } else {
-                        $id = 5;
-                        $etat = $etatRepository->find($id);
-                        $e->setEtat($etat);
-                        $entityManager->persist($e);
-                        $entityManager->flush();
+                    if ($today > $eventDate)
+                    {
+                        if ($today < $eventDate->modify('+ ' . $e->getDuration() . 'days'))
+                        {
+                            $id = 4;
+                            $etat = $etatRepository->find($id);
+
+                            $e->setEtat($etat);
+                            $entityManager->persist($e);
+                            $entityManager->flush();
+                        } else
+                        {
+                            $id = 5;
+                            $etat = $etatRepository->find($id);
+                            $e->setEtat($etat);
+                            $entityManager->persist($e);
+                            $entityManager->flush();
+                        }
+                    }
+                    if ($today < $eventDate)
+                    {
+                        if ($today < $limitDate)
+                        {
+                            $id = 2;
+                            $etat = $etatRepository->find($id);
+                            $e->setEtat($etat);
+                            $entityManager->persist($e);
+                            $entityManager->flush();
+                        } else
+                        {
+                            $id = 3;
+                            $etat = $etatRepository->find($id);
+                            $e->setEtat($etat);
+                            $entityManager->persist($e);
+                            $entityManager->flush();
+                        }
                     }
                 }
-                if ($today < $eventDate) {
-                    if ($today < $limitDate) {
-                        $id = 2;
-                        $etat = $etatRepository->find($id);
-                        $e->setEtat($etat);
-                        $entityManager->persist($e);
-                        $entityManager->flush();
-                    } else {
-                        $id = 3;
-                        $etat = $etatRepository->find($id);
-                        $e->setEtat($etat);
-                        $entityManager->persist($e);
-                        $entityManager->flush();
-                    }
+
+                if($today> $eventDate->modify( '+' .$archiveDate))
+                {
+                    $id = 7;
+                    $etat = $etatRepository->find($id);
+                    $e->setEtat($etat);
+                    $entityManager->persist($e);
+                    $entityManager->flush();
+//                     $myVariableCSV = "id; name; startingDate; duration; limiteInscribeDate; maxInscriptionsNumber; information; organisator; campus; etat; participants\n";
+//                     //Ajout de données (avec le . devant pour ajouter les données à la variable existante)
+//                     $myVariableCSV .= serialize($e);
+//                     //On donne la variable en string à la response, nous définissons le code HTTP à 200
+//
+//                return new Response
+//                    (
+//                        $myVariableCSV,
+//                        200,
+//                        [
+//                            //Définit le contenu de la requête en tant que fichier Excel
+//                            'Content-Type' => 'application/vnd.ms-excel',
+//                            //On indique que le fichier sera en attachment donc ouverture de boite de téléchargement ainsi que le nom du fichier
+//                            "Content-disposition" => "filename=backup.csv"
+//                        ]
+//                    );
+}
                 }
-            }
-        }
+
         return $this->render('main/home.html.twig',
             compact("events")
         );
