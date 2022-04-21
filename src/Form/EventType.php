0b47496ff20ae4b153c65b49dd9abce506fb2234
kgardie2021 <kevin.gardie2021@campus-eni.fr>
2022-04-21 17:00:02 +0200

    Form clean

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -2,20 +2,11 @@
 
 namespace App\Form;
 
-
 use App\Entity\Campus;
-use App\Entity\City;
 use App\Entity\Etat;
 use App\Entity\Event;
-use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
-use Doctrine\ORM\Mapping\Entity;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
-use SebastianBergmann\CodeCoverage\Report\Text;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\ChoiceList\ChoiceList;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -25,14 +16,13 @@ class EventType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('campus', EntityType::class, ['class' => Campus::class, 'choice_label'=> 'name'])
+            ->add('campus', EntityType::class, ['class' => Campus::class, 'choice_label' => 'name'])
             ->add('startingDate')
             ->add('duration')
             ->add('limitInscribeDate')
             ->add('maxInscriptionsNumber')
             ->add('informations')
-            ->add('etat',EntityType::class, ['class'=>Etat::class, 'choice_label'=>'libelle'] )
-        ;
+            ->add('etat', EntityType::class, ['class' => Etat::class, 'choice_label' => 'libelle']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,67 +3,41 @@
 namespace App\Form;
 
 use App\Entity\User;
-use App\Repository\EventRepository;
-use SebastianBergmann\CodeCoverage\Report\Text;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options ): void
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
 
         $builder
-            ->add('email', TextType::class, array('attr'=>array('class'=>'input', 'type'=>'email', 'placeholder'=>'alexsmith@gmail.com')))
-            ->add('firstname', TextType::class, array('attr'=>array('class'=>'input', 'type'=>'text', 'placeholder'=>'Yvon')))
-            ->add('lastname', TextType::class, array('attr'=>array('class'=>'input', 'type'=>'text', 'placeholder'=>'Lavaler')))
-            ->add('pseudo', TextType::class, array('attr'=>array('class'=>'input', 'type'=>'text', 'placeholder'=>'BigBossDu44')))
-            ->add('phone', TextType::class, array('attr'=>array('class'=>'input', 'type'=>'text', 'placeholder'=>'Fait un effort ...')))
-            ->add('photo',FileType::class,
-            [
-                'mapped'=>false,
-                'required'=>false,
-                'constraints'=>[
-                    new File([
-                        'maxSize'=>'1024k',
-                        'mimeTypes'=>[
-                            'image/gif',
-                            'image/jpeg',
-                            'image/jpg',
-                            'image/png',
+            ->add('email', TextType::class, array('attr' => array('class' => 'input', 'type' => 'email', 'placeholder' => 'alexsmith@gmail.com')))
+            ->add('firstname', TextType::class, array('attr' => array('class' => 'input', 'type' => 'text', 'placeholder' => 'Yvon')))
+            ->add('lastname', TextType::class, array('attr' => array('class' => 'input', 'type' => 'text', 'placeholder' => 'Lavaler')))
+            ->add('pseudo', TextType::class, array('attr' => array('class' => 'input', 'type' => 'text', 'placeholder' => 'BigBossDu44')))
+            ->add('phone', TextType::class, array('attr' => array('class' => 'input', 'type' => 'text', 'placeholder' => 'Fait un effort ...')))
+            ->add('photo', FileType::class,
+                [
+                    'mapped' => false,
+                    'required' => false,
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '1024k',
+                            'mimeTypes' => [
+                                'image/gif',
+                                'image/jpeg',
+                                'image/jpg',
+                                'image/png',
                             ],
-                        'mimeTypesMessage'=>'Choose a valid format : gif,jpeg,jpg,png',
-                    ])
-                ]
-            ])
-
-//            ->add('plainPassword', PasswordType::class, [
-//                // instead of being set onto the object directly,
-//                // this is read and encoded in the controller
-//                'mapped' => false,
-//                'attr' => ['autocomplete' => 'new-password'],
-//                'constraints' => [
-//                    new NotBlank([
-//                        'message' => 'Please enter a password',
-//                    ]),
-//                    new Length([
-//                        'min' => 6,
-//                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-//                        // max length allowed by Symfony for security reasons
-//                        'max' => 4096,
-//                    ]),
-//                ],
-//            ])
-        ;
+                            'mimeTypesMessage' => 'Choose a valid format : gif,jpeg,jpg,png',
+                        ])
+                    ]
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
