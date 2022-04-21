65008ad90be7c27642026fd064a725f72774c414
kgardie2021 <kevin.gardie2021@campus-eni.fr>
2022-04-21 16:56:43 +0200

    Controller/Admin clean

--- src/Controller/Admin/CampusCrudController.php
+++ src/Controller/Admin/CampusCrudController.php
@@ -5,8 +5,6 @@ namespace App\Controller\Admin;
 use App\Entity\Campus;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CampusCrudController extends AbstractCrudController
@@ -19,7 +17,6 @@ class CampusCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            //IdField::new('id'),
             TextField::new('name'),
             AssociationField::new('users'),
         ];
