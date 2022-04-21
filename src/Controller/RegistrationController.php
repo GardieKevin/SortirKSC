b3d146a8f219e55ed3ab2ec3b67cd42ed7ce16c3
selie2021 <sebastien.elie@hotmail.fr>
2022-04-21 14:33:25 +0200

    Seb maj envoi mail apres register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use App\Entity\ResetPasswordRequest;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\ResetPasswordRequestRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 
 class RegistrationController extends AbstractController
@@ -31,7 +34,7 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SluggerInterface $slugger, ResetPasswordController $resetPasswordController, MailerInterface $mi, TranslatorInterface $tr): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -43,40 +46,30 @@ class RegistrationController extends AbstractController
             // encode the plain password
             $password=$this->Genere_Password(10);
             $user->setPassword($password);
-//            $userPasswordHasher->hashPassword(
-//                    $user,
-//                    $form->get('plainPassword')->getData()
-//
-//            );
 
             $photo = $form->get('photo')->getData();
 
-            // this condition is needed because the 'brochure' field is not required
-            // so the PDF file must be processed only when a file is uploaded
             if ($photo) {
                 $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
                 // this is needed to safely include the file name as part of the URL
                 $safeFilename = $slugger->slug($originalFilename);
                 $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
 
-                // Move the file to the directory where brochures are stored
                 try {
                     $photo->move(
                         $this->getParameter('user_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {
-                    // ... handle exception if something happens during file upload
                 }
-
-                // updates the 'brochureFilename' property to store the PDF file name
-                // instead of its contents
                 $user->setImage($newFilename);
             }
-
             $entityManager->persist($user);
             $entityManager->flush();
-
+            $emailUser=$user->getEmail();
+            $mailer=$mi;
+            $translator=$tr;
+            $resetPasswordController->processSendingPasswordResetEmail($emailUser, $mailer, $translator)->send();
             // do anything else you need here, like send an email
 
             return $this->redirectToRoute('main_home');
